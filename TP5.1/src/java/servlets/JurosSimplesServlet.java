3131b8a4df32f62ee4c47bcf06cb1bfe1c153d25
AngeloMaverick <angelo.santos4@fatec.sp.gov.br>
2021-03-08 20:52:42 -0300

    TP5.1
    Atividade Prática

---
+++ TP5.1/src/java/servlets/JurosSimplesServlet.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Angelo Marcio Sousa Santos
+ */
+@WebServlet(name = "JurosSimplesServlet", urlPatterns = {"/juros-simples.html"})
+public class JurosSimplesServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>JurosSimplesServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h3><a href='index.html'>Voltar</a></h3>");
+            out.println("<h1>Juros Simples Servlet</h1>");
+            try{
+                double n1 = Double.parseDouble(request.getParameter("n1"));
+                double n2 = Double.parseDouble(request.getParameter("n2"));
+                n2 = n2/100;
+                double n3 = Integer.parseInt(request.getParameter("n3"));
+                double vf = n1*(1+n2*n3);
+                out.println("<h2>VP*(1+j*n)=VF</h2>");
+                out.println("<h2>"+n1+"*(1+"+n2+"*"+n3+")=VF</h2>");
+                out.println("<h2>"+n1+"*(1+"+n2+"*"+n3+")="+vf+"</h2>");
+                out.println("<h2>Valor Futuro[VF]= "+vf+"</h2>");
+            }catch(Exception ex){
+                out.println("<h2>Ocorreu um erro: "+ex.getLocalizedMessage()+"</h2>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
